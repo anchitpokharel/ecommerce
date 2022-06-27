f77e748c0ca9237a903f87d5b5839167cf279bb9
Anchit Pokharel <pokharelanchit@gmail.com>
2022-06-27 14:11:55 +0545

    query_set

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http    import HttpResponse
-
+from store.models import Product
 # Create your views here.
 
 def say_hello(request):
+    query_set = Product.objects.count()
+    
     return render(request, 'hello.html', {'title' : 'oooooooooooooooooooooooooooooooo'})
