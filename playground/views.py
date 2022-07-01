1de13954b2ae84bfc1793c4630ed0be350dd065a
Anchit Pokharel <pokharelanchit@gmail.com>
2022-07-01 07:35:19 +0545

    prefetch_related and select related

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http    import HttpResponse
-from store.models import Product
+from django.db.models import Q, F
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+
+from store.models import OrderItem, Product
 # Create your views here.
 
 def say_hello(request):
-    query_set = Product.objects.count()
-    
-    return render(request, 'hello.html', {'title' : 'oooooooooooooooooooooooooooooooo'})
+    # select_related (1)
+    # prefetch_related (2)
+    product = Product.objects.prefetch_related('promotions').all()
+    return render(request, 'hello.html', {'title' : 'oooooooooooooooooooooooooooooooo', 'product': list(product)})
